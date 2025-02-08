68189de93533a8606b4350e29429288e6ef01fb8
Krishanpal Singh <ksolanki1306@gmail.com>
2025-02-08 18:55:15 +0530

    Initial commit

---
+++ src/main/java/com/productservice/ProductServiceConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.productservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductServiceConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductServiceConsumerApplication.class, args);
+	}
+
+}
